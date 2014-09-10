e171f0753265324ff133fff3fcac04ee6b748296
Deep Singh <deepam1982@gmail.com>
2014-09-10 12:54:55 +0530

    communicator selection is now config driven

--- main.js
+++ main.js
@@ -10,86 +10,90 @@ if(noLogs) {
 
 
 __rootPath = __dirname;
-var groupConfig = require(__rootPath+"/classes/configs/groupConfig");
 var BasicConfigManager = require(__rootPath+"/classes/configs/basicConfigManager");
-__remoteDevInfoConf = require(__rootPath+"/classes/configs/deviceInfoConfig");
-var UsrCnfMngr = BasicConfigManager.extend({file : '/../configs/userConfig.json'});
-__userConfig = new UsrCnfMngr({'callback':function (err) {
-  __userEmail = __userConfig.get('email');
-  var express = require('express');
-  var favicon = require('serve-favicon');
-  //var expressDevice = require('express-device');
-  var app = express()
-    , server = require('http').createServer(app)
-    , io = require('socket.io').listen(server)
+var SystemConfigMngr = BasicConfigManager.extend({file : '/../configs/systemConfig.json'});
+__systemConfig = new SystemConfigMngr({'callback':function(err){
+        var groupConfig = require(__rootPath+"/classes/configs/groupConfig");
+        __remoteDevInfoConf = require(__rootPath+"/classes/configs/deviceInfoConfig");
 
-  //io.set('log level', 1);
-  server.listen(80);
+        var UsrCnfMngr = BasicConfigManager.extend({file : '/../configs/userConfig.json'});
+        __userConfig = new UsrCnfMngr({'callback':function (err) {
+          __userEmail = __userConfig.get('email');
+          var express = require('express');
+          var favicon = require('serve-favicon');
+          //var expressDevice = require('express-device');
+          var app = express()
+            , server = require('http').createServer(app)
+            , io = require('socket.io').listen(server)
 
-  app.use(favicon(__rootPath + '/static/images/favicon.ico'));
-  //app.use('/favicon', express.static(__rootPath + '/static/images', {maxAge:86400}));
-  app.use('/static/images', express.static(__rootPath + '/static/images', {maxAge:86400}));
-  app.use('/static', express.static(__rootPath + '/static'));
-  //app.use(expressDevice.capture());
-  app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.send(500, 'Something broke!');
-  });
+          //io.set('log level', 1);
+          server.listen(80);
 
-  var websiteRoutes = require(__rootPath + '/routes/website')
-  websiteRoutes(app);
+          app.use(favicon(__rootPath + '/static/images/favicon.ico'));
+          //app.use('/favicon', express.static(__rootPath + '/static/images', {maxAge:86400}));
+          app.use('/static/images', express.static(__rootPath + '/static/images', {maxAge:86400}));
+          app.use('/static', express.static(__rootPath + '/static'));
+          //app.use(expressDevice.capture());
+          app.use(function(err, req, res, next){
+            console.error(err.stack);
+            res.send(500, 'Something broke!');
+          });
 
-  var checkConfigurations = function (socket){
-    if(!__userConfig.get('zigbeeNetworkName') || !__userConfig.get('zigbeeNetworkKey'))
-      socket.emit('switchPage', 'networkSetting');
-    else if(!__userConfig.get('email') || !__userConfig.get('password'))
-      socket.emit('switchPage', 'cloudSetting');
-    else if(!__.keys(__remoteDevInfoConf.data).length)
-      socket.emit('switchPage', 'configureModule');
-  }
-  io.sockets.on('connection', function (socket) {
-    console.log('Socket connection established!!');
-    socket.on('checkConfigurations', __.bind(checkConfigurations,null, socket));
-    checkConfigurations(socket);
-  });
-  setInterval(function () {io.sockets.emit('sudoHeartbeat')}, 1000);
+          var websiteRoutes = require(__rootPath + '/routes/website')
+          websiteRoutes(app);
+
+          var checkConfigurations = function (socket){
+            if(!__userConfig.get('zigbeeNetworkName') || !__userConfig.get('zigbeeNetworkKey'))
+              socket.emit('switchPage', 'networkSetting');
+            else if(!__userConfig.get('email') || !__userConfig.get('password'))
+              socket.emit('switchPage', 'cloudSetting');
+            else if(!__.keys(__remoteDevInfoConf.data).length)
+              socket.emit('switchPage', 'configureModule');
+          }
+          io.sockets.on('connection', function (socket) {
+            console.log('Socket connection established!!');
+            socket.on('checkConfigurations', __.bind(checkConfigurations,null, socket));
+            checkConfigurations(socket);
+          });
+          setInterval(function () {io.sockets.emit('sudoHeartbeat')}, 1000);
 
 
-  var SocketCommandManager = require(__rootPath + '/classes/sockets/commandManager')
-  var socComMngr = new SocketCommandManager({'localIo':io});
+          var SocketCommandManager = require(__rootPath + '/classes/sockets/commandManager')
+          var socComMngr = new SocketCommandManager({'localIo':io});
 
-  var SocketRequestManager = require(__rootPath + '/classes/sockets/requestManager')
-  var socReqMngr = new SocketRequestManager({'localIo':io});
+          var SocketRequestManager = require(__rootPath + '/classes/sockets/requestManager')
+          var socReqMngr = new SocketRequestManager({'localIo':io});
 
-  var SocketEditManager = require(__rootPath + '/classes/sockets/editManager')
-  var socEdtMngr = new SocketEditManager({'localIo':io});
-  
-  var deviceManager = require(__rootPath + '/classes/devices/deviceManager');
-  //var roomModel = require(__rootPath+"/configs/managers/roomConfigManager");
+          var SocketEditManager = require(__rootPath + '/classes/sockets/editManager')
+          var socEdtMngr = new SocketEditManager({'localIo':io});
+          
+          var deviceManager = require(__rootPath + '/classes/devices/deviceManager');
+          //var roomModel = require(__rootPath+"/configs/managers/roomConfigManager");
 
-  deviceManager.on('deviceStateChanged', function () {
-    io.sockets.emit('roomConfigUpdated', groupConfig.getList())
-  });
+          deviceManager.on('deviceStateChanged', function () {
+            io.sockets.emit('roomConfigUpdated', groupConfig.getList())
+          });
 
-  require(__rootPath + '/classes/sockets/initClientSocket')(function (err, cloudSocket) {
-    cloudSocket.emit('roomConfigUpdated', groupConfig.getList())
-    deviceManager.on('deviceStateChanged', function () {
-      cloudSocket.emit('roomConfigUpdated', groupConfig.getList())
-    });  
-    socComMngr.setCloudSocket(cloudSocket);
-    socReqMngr.setCloudSocket(cloudSocket);
-  })
-    
+          require(__rootPath + '/classes/sockets/initClientSocket')(function (err, cloudSocket) {
+            cloudSocket.emit('roomConfigUpdated', groupConfig.getList())
+            deviceManager.on('deviceStateChanged', function () {
+              cloudSocket.emit('roomConfigUpdated', groupConfig.getList())
+            });  
+            socComMngr.setCloudSocket(cloudSocket);
+            socReqMngr.setCloudSocket(cloudSocket);
+          })
+            
 
-  var coordinator = require(__rootPath+"/classes/coordinator");
-  // coordinates between sensors, virtual devices, timers etc.
-  console.log("main.js ran");
+          var coordinator = require(__rootPath+"/classes/coordinator");
+          // coordinates between sensors, virtual devices, timers etc.
+          console.log("main.js ran");
 
-  var eventLogger = require(__rootPath+"/classes/eventLogger/logger");
-  eventLogger.addEvent("homeControllerStarted");
+          var eventLogger = require(__rootPath+"/classes/eventLogger/logger");
+          eventLogger.addEvent("homeControllerStarted");
 
-  process.on('uncaughtException', function (err) {
-      console.log( "UNCAUGHT EXCEPTION " );
-      console.log( "[Inside 'uncaughtException' event] " + err.stack || err.message );
-  });
+          process.on('uncaughtException', function (err) {
+              console.log( "UNCAUGHT EXCEPTION " );
+              console.log( "[Inside 'uncaughtException' event] " + err.stack || err.message );
+          });
+        }});
 }});
