af1f696b8515f8c7601b44b319cdc68433a243a4
Deep Singh <deepam1982@gmail.com>
2015-05-13 11:52:16 +0530

    shallIRestart script, which runs every 15 mins

--- shellScripts/shallRestart.sh
+++ shellScripts/shallRestart.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+DIR0=/home/admin/inoho
 
 function jsonval {
     temp=`echo $json | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $prop| cut -d":" -f2| sed -e 's/^ *//g' -e 's/ *$//g'`
     echo ${temp##*|}
 }
 # https://gist.github.com/cjus/1047794
-
+echo `date`
 email='unconfigured@inoho.com'
-if [ -f ~/inoho/configs/userConfig.json ]; then
-	json=$(<~/inoho/configs/userConfig.json)
+if [ -f $DIR0/configs/userConfig.json ]; then
+	json=$(<$DIR0/configs/userConfig.json)
 	prop='email'
 	email=`jsonval`
 fi
@@ -24,7 +25,7 @@ success=`jsonval`
 #echo $success
 #echo $restart
 
-if [ "$success" == "true" -a "$restart" == "false" ]; then
+if [ "$success" == "true" -a "$restart" == "true" ]; then
 	CMD="sudo service inoho restart"
 	eval "$CMD"
 fi
